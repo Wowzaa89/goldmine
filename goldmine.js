9b09d38c8b678a6d59a40881c4ff22925aac7917
Wowzaa89 <121795435+Wowzaa89@users.noreply.github.com>
2025-09-08 19:40:25 -0500

    Create goldmine.js

---
+++ goldmine.js
@@ -0,0 +1,13 @@
+// GoldMine Mod
+// By Wowzaa89
+elements.bizare_bomb = {
+    color: "#ffffff",
+    name: "BizareBomb",
+    behavior: [
+        "XX|CR:smoke|XX",
+        "XX|XX|XX",
+        "XX|M1 AND EX:50>fire|XX"
+    ],
+    state: "solid",
+    category: "weapon"
+}
